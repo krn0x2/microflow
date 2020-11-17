a8b120db5db41cb70a1022d1389cb2dc38ddbaef
Karan Chhabra <krn0x2@gmail.com>
2020-11-17 17:59:56 +0100

    Remove lib from git, contructor config union null

--- lib/interpreter/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Interpreter, StateSchema, EventData, Typestate, State } from 'xstate';
-import { WorkflowEvent } from '../types';
-export declare class WorkflowInterpreter extends Interpreter<any, StateSchema, WorkflowEvent, Typestate<any>> {
-    mSend(event: WorkflowEvent, payload?: EventData): State<any, WorkflowEvent, StateSchema, Typestate<any>>;
-}
--- lib/utils/index.d.ts
+++
@@ -1,3 +0,0 @@
-declare const transform: (obj: any, root: any) => any;
-declare const setOnPath: (root: any, path: string, obj: any) => any;
-export { transform, setOnPath };
